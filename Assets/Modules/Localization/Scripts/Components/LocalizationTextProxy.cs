5e8a3fb0fa1a90c7fda341f5a1940dc751232391
alexander.danilovski <a.danilovski@starkitstudio.com>
2025-05-19 16:33:57 +0300

    start work on Module.Localization

---
+++ Assets/Modules/Localization/Scripts/Components/LocalizationTextProxy.cs
@@ -0,0 +1,42 @@
+﻿using Modules.Localization.Scripts.Core;
+using TMPro;
+using UnityEngine;
+
+namespace Modules.Localization.Scripts.Components
+{
+    public abstract class LocalizationTextProxy : MonoBehaviour
+    {
+        protected bool _isInit;
+        protected LocalizationManager _manager;
+        protected TextMeshProUGUI _textComponent;
+
+        private void Awake()
+        {
+            Init();
+        }
+
+        private void OnEnable()
+        {
+            if (!_isInit)
+                return;
+
+            _manager.OnChangeLanguage += OnChangeLanguageHandler;
+        }
+
+        private void OnDisable()
+        {
+            if (!_isInit)
+                return;
+
+            _manager.OnChangeLanguage -= OnChangeLanguageHandler;
+        }
+
+        private void OnChangeLanguageHandler(SystemLanguage _)
+        { 
+            //TODO: ...
+            //...
+        }
+
+        protected abstract void Init(); //_localization = ProjectContext.Instance.Container.Resolve<Localization>();
+    }
+}
---
+++ Assets/Modules/Localization/Scripts/Core/LocalizationManager.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using UnityEngine;
+
+namespace Modules.Localization.Scripts.Core
+{
+    public class LocalizationManager
+    {
+        public Action<SystemLanguage> OnChangeLanguage;
+
+
+        public void Init()
+        {
+            //UnityEngine.Application.systemLanguage
+
+            //...
+        }
+
+
+        public bool TrySetLanguage(SystemLanguage newLanguage)
+        {
+            //...
+
+            return false;
+        }
+
+        public string GetString(string key)
+        {
+            //...
+
+            return string.Empty;
+        }
+
+
+        private void ApplyLanguage()
+        { 
+            //...
+        }
+
+        private void LoadLanguage(SystemLanguage language)
+        { 
+            //...
+        }
+    }
+}
